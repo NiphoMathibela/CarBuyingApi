f4a36134ae0eaecefd1ab214638e3f4e16b4289b
Nhlakanipho Mathibela <nmathibela2801@gmail.com>
2023-09-23 23:53:03 +0200

    Added DB settings model

---
+++ CarBuyingApi/Models/CarsForSaleDbSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBuyingApi.Models
+{
+    public class CarsForSaleDbSettings
+    {
+        public string ConnectionSring { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string CarsCollectionName { get; set; } = null!;
+    }
+}
