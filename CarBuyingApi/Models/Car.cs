729f756c30f9d303c1c94cdebedb9f7b2067ae78
Nhlakanipho Mathibela <nmathibela2801@gmail.com>
2023-09-23 23:35:02 +0200

    Added car model

---
+++ CarBuyingApi/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CarBuyingApi.Models
+{
+    public class Car
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string CarNameee { get; set; } = null!;
+
+        public string Brand { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string GearType { get; set; } = null!;
+
+        public string Engine { get; set; } = null!;
+
+        public string EngineSize { get; set; } = null!;
+
+        public string Power { get; set; } = null!;
+
+        public decimal FuelCons {  get; set; }
+
+        public string Seats { get; set; } = null!;
+
+    }
+}
